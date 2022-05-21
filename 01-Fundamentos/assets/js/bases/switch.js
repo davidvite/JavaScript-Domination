9f2c43f50db3504c0633406a12c8c47402d913f2
David Vite <davidvite@users.noreply.github.com>
2022-05-20 21:08:28 -0600

    ciclos y mas bases

---
+++ 01-Fundamentos/assets/js/bases/switch.js
@@ -0,0 +1,18 @@
+
+
+const dia= 3;
+
+switch(dia){ // es === 
+ case 0:
+     console.log('Domingo');
+     break;
+ case 1:
+     console.log('Lunes')
+     break;
+ case 2:
+     console.log('Martes')
+     break;
+
+    default:
+        console.log('No es L , M, D');
+}
---
+++ 01-Fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,30 @@
+const carros = ['Ford' , 'Mazda', 'Honda', 'Toyota'];
+
+console.warn('while')
+
+let i =0;
+// while(i < carros.length ){
+//     console.log(carros[i]);
+//     i++;
+// }
+
+//undefined
+//null
+//false
+
+while(carros[i] ){
+    if(i === 1){
+        i++;
+        continue;
+    }
+    console.log(carros[i]);
+    i++;
+}
+
+
+console.warn('Do while');
+let j = 0;
+do{
+   console.log(carros[j]) 
+   j++
+}while(carros[j]);
