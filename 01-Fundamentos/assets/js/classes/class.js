777a55a11a4fcff59c98c41bf471d9ecd451d75f
David Vite <davidvite@users.noreply.github.com>
2022-05-31 15:40:42 -0600

    Mas practica con clases.

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -1,14 +1,29 @@
 class Persona {
-    nombre;//En la inicializacion de propiedades no podemos usar , para iniciar muchas por eso usamos ; 
-    codigo;
-    frase;
+nombre;//En la inicializacion de propiedades no podemos usar , para iniciar muchas por eso usamos ; 
 
+     static _conteo = 0;
+     static get conteo() {
+         return Persona._conteo + ' instancias';
+     }
+     static mensaje(){
+         console.log(this.nombre); //Undefined
+         console.log('Hola soy metodo estatico')
+     }
     constructor(nombre , codigo, frase) { //Metodo que se va a ejecutar exactamente al momento de crear una nueva instancia de Persona
 
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo ++; 
     }// El constructor es el unico obnjeto que no regresa undefined y siempre va a regresar la instancia del objeto
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return`La comida Favorita de ${this.nombre} es ${this.comida}`;
+    }
 
     quienSoy(){
         console.log(`soy ${this.nombre} y mi identidad es ${this.codigo}`)
@@ -24,8 +39,24 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spider', 'Soy ty amigable vecino Spiderman');
 const ironman   = new Persona('Tonny Stark', 'Ironman', 'I am Tonny Stark');
-console.log(spiderman , ironman);
 
-spiderman.quienSoy();
+
+
 spiderman.miFrase();
-ironman.quienSoy();
+spiderman.setComidaFavorita = 'El pay de cereza de la tia may';
+spiderman.nemesis ='Duende Verde' // No hacer esto por que no va a estar en todas las instancias de personajes solo en Spiderman
+
+
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman);
+// Persona._conteo = 2;
+
+console.log('Conteo estatico', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna ='Hola Mundo';
+
+console.log(Persona)
+console.log(Persona.propiedadExterna);
+
