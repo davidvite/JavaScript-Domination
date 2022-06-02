0036eacfe0e0cbeaa6255a474a85ed856e09014c
David Vite <davidvite@users.noreply.github.com>
2022-06-02 13:29:43 -0600

    Mas practicas con webpack y configuracion de ambientes de desarrollo

--- 03-webpack-inicial/webpack.prod.js
+++ 03-webpack-inicial/webpack.prod.js
@@ -2,12 +2,16 @@ const HtmlWebpack    = require('html-webpack-plugin')
 const MiniCssExtract = require('mini-css-extract-plugin');
 const CopyPlugin     = require("copy-webpack-plugin");
 
+const Terser         = require("terser-webpack-plugin");
+const CssMinimizer   = require("css-minimizer-webpack-plugin");
+
 module.exports = {
     
-    mode: "development",
+    mode: "production",
 
     output: {
-        clean: true
+        clean: true,
+        filename: 'main.[constenthas].js'
     },
 
     module: {
@@ -31,11 +35,29 @@ module.exports = {
             {
                 test: /\.(png|jpe?g|gif)$/,
                 loader: 'file-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
             }
+            
         ]
     },
 
-    optimization: {},
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser(),
+        ]
+
+    },
 
     plugins: [
         new HtmlWebpack({
@@ -45,7 +67,7 @@ module.exports = {
         }),
         
         new MiniCssExtract({
-            filename: '[name].css',
+            filename: '[name].[fullhash].css',
             ignoreOrder: false
         }),
 
